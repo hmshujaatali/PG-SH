256241f3e944fc8536078150862874aeac0f19b3
hmshujaatali <h.m.shujaatali786@gmail.com>
2021-04-22 09:08:45 +0500

    only head edited

---
+++ app.js
@@ -0,0 +1,14 @@
+var a = +prompt("Enter your Marks from 1-100")
+if (a >= 70) {
+    document.write("your grade is A")
+} else if (a >= 60 & a <= 69) {
+    document.write("your grade is B")
+
+} else if (a >= 40 & a <= 59) {
+    document.write("your grade is C")
+
+} else {
+    document.write("YOU ARE FAIL")
+    alert("TRY AGAIN")
+
+}
